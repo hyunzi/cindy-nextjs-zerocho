213b17fda74400151169b9bb4c48f17bca481344
hyyunzi <hertz.insight@gmail.com>
2025-05-11 15:42:20 +0900

    [Section 3] explore, profile 페이지 구현

--- src/app/(afterLogin)/explore/page.tsx
+++ src/app/(afterLogin)/explore/page.tsx
@@ -1,7 +1,21 @@
+import SearchForm from "@/app/(afterLogin)/_component/SearchForm";
+import style from "@/app/(afterLogin)/explore/explore.module.css";
+import Trend from "@/app/(afterLogin)/_component/Trend";
+
 export default function Page() {
-  return (
-    <main>
-      탐색
-    </main>
-  )
+    return (
+        <main className={style.main}>
+            <div className={style.formZone}>
+                <SearchForm/>
+            </div>
+            <div className={style.trend}>
+                <h3>나를 위한 트렌드</h3>
+                <Trend/>
+                <Trend/>
+                <Trend/>
+                <Trend/>
+                <Trend/>
+            </div>
+        </main>
+    )
 }
